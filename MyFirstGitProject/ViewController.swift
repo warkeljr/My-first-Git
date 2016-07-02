df6e9639fb280172b9c61326c2d80baf0416fdd1
W.S. van Arkel jr <warkel@imac-van-ws.local>
2016-07-02 15:35:34 +0200

    Initial commit

--- MyFirstGitProject/ViewController.swift
+++ MyFirstGitProject/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    let myGitProject = "My First Git project"
+    let myGitProject = "My First Git project, yes i did it"
 
     override func viewDidLoad() {
         super.viewDidLoad()
